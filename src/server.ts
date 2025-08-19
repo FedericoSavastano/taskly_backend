b3bd1a70e8aac553634dde697f10d9dae8d528f7
FedericoSavastano <f.savastano@hotmail.com>
2025-08-19 20:27:34 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,36 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import morgan from 'morgan';
-import { corsConfig } from './config/cors';
-import { connectDB } from './config/db';
-import authRoutes from './routes/authRoutes';
-import projectRoutes from './routes/projectRoutes';
-import swaggerUi from 'swagger-ui-express';
-import swaggerSpec, { swaggerUiOptions } from './config/swagger';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+import { corsConfig } from "./config/cors";
+import { connectDB } from "./config/db";
+import authRoutes from "./routes/authRoutes";
+import projectRoutes from "./routes/projectRoutes";
+import swaggerUi from "swagger-ui-express";
+import swaggerSpec, { swaggerUiOptions } from "./config/swagger";
 
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors(corsConfig));
+//app.use(cors(corsConfig));
+app.use(cors());
 
 // Logging
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // For reading form data
 app.use(express.json());
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/projects', projectRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/projects", projectRoutes);
 
 //Generates documentation of endpoints in swagger platform
 app.use(
-    '/docs',
-    swaggerUi.serve,
-    swaggerUi.setup(swaggerSpec, swaggerUiOptions)
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpec, swaggerUiOptions)
 );
 
 export default app;
